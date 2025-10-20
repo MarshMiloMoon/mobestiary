fbca0e8258858e3dfcb38a8811075077f8e9045e
thomas-milo-moon <thomas_milo_moon01@student.smc.edu>
2025-10-19 20:53:50 -0700

    stuff (gradle still not working)

--- src/main/java/net/marshmilomoon/mobestiary/entity/ModEntities.java
+++ src/main/java/net/marshmilomoon/mobestiary/entity/ModEntities.java
@@ -5,15 +5,19 @@ import net.marshmilomoon.mobestiary.entity.custom.ClayGolemEntity;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
 
 public class ModEntities {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
             DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, Mobestiary.MOD_ID);
 
-    public static final EntityType<ClayGolemEntity> CLAY_GOLEM = Registry.register(
-            Registries.ENTITY_TYPE, new Identifier()
-    )
+    public static final Supplier<EntityType<ClayGolemEntity>> CLAY_GOLEM =
+            ENTITY_TYPES.register("clay_golem", () -> EntityType.Builder.of(ClayGolemEntity::new, MobCategory.MISC)
+                    .sized(1f, 1.125f).build("clay_golem"));
 
     public static void register(IEventB eventBus) {
 
