4874f5e6b0306e7e802f6d6b26b245f47b70d569
thomas-milo-moon <thomas_milo_moon01@student.smc.edu>
2025-10-30 14:48:10 -0700

    Added Clay Golem and its spawn egg

---
+++ src/main/java/net/marshmilomoon/mobestiary/event/ModClientEvents.java
@@ -0,0 +1,10 @@
+package net.marshmilomoon.mobestiary.event;
+
+import net.marshmilomoon.mobestiary.Mobestiary;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = Mobestiary.MOD_ID, value = Dist.CLIENT)
+public class ModClientEvents {
+
+}
---
+++ src/main/java/net/marshmilomoon/mobestiary/event/ModEvents.java
@@ -0,0 +1,9 @@
+package net.marshmilomoon.mobestiary.event;
+
+import net.marshmilomoon.mobestiary.Mobestiary;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = Mobestiary.MOD_ID)
+public class ModEvents {
+
+}
